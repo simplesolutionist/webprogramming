edaaebfdc3d46252f3c9d7547f805bacc0aa7bbd
simplesolutionist <aymanali180@gmail.com>
2020-10-10 20:37:34 -0400

    added basic design elements for feed

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/login', name: 'Login', component: Login },
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/feed',
+    name: 'Feed',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Feed.vue')
   },
   {
     path: '/about',
@@ -17,7 +20,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
 ]
 
 const router = new VueRouter({
