f7bdf83ab79cff61ea5415fda275c72fa5f00864
simplesolutionist <aymanali180@gmail.com>
2020-10-13 19:07:08 -0400

    More Vue examples

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -27,7 +27,7 @@
         </div>
         <div class="field">
         <p class="control">
-            <button class="button is-success">
+            <button class="button is-success" @click.prevent="login">
             Login
             </button>
         </p>
@@ -38,7 +38,19 @@
 </template>
 
 <script>
+import session from "@/models/session";
 export default {
+    methods: {
+        login(){
+            session.user = {
+                name: 'Ayman Ali',
+                handle: 'simplesolutionist',
+                profile: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/120552651_10102367831029710_8523278423201553541_n.jpg?_nc_cat=103&_nc_sid=09cbfe&_nc_ohc=Jnr3FRfM4FIAX8F5rC9&_nc_ht=scontent-lga3-1.xx&oh=14a477b3f03186158eccf30fe3a03bda&oe=5FA552C2'
+            }
+            session.addNotification('Yay! You logged in', 'success')
+            this.$router.push('feed')
+        }
+    }
 }
 </script>
 
