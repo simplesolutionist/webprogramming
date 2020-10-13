d7fa4b72a6ae5f043220d914c4a47764a532f32a
simplesolutionist <aymanali180@gmail.com>
2020-10-13 19:07:51 -0400

    More Vue Examples

--- client/src/views/Feed.vue
+++ client/src/views/Feed.vue
@@ -6,9 +6,10 @@
             <div class="column is-two-thirds">
                 <Post v-for=" (x, i) in posts " 
                       :key="i"
+                      :i="i"
                       :post="x" /> 
             </div>
-            <div class="column is-one-third">
+            <div class="column is-one-third" @mouseenter="error">
                 <Sidebar />
             </div>
         </div>
@@ -19,6 +20,7 @@
 import Sidebar from "@/components/Sidebar";
 import Post from "@/components/Post";
 import { posts } from "@/models/feed";
+import session from "@/models/session";
 export default {
     data(){
         return {
@@ -27,6 +29,11 @@ export default {
     },
     components: {
         Sidebar, Post
+    },
+    methods: {
+        error(){
+            session.addNotification('Something went wrong.', 'danger')
+        }
     }
 }
 </script>
